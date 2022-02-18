2b26b69b1f007e75a116757fc85cce12bef19ee8
Daniil Batanin <pikabu1305@gmail.com>
2022-02-18 15:17:14 +0300

    fix and add headex, about us

---
+++ src/components/Navbar/Links.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Links = (props) => {
+  return (
+    <ul className = {props.className}>
+        <li className='p__opensans'><a href='#home'>Home</a></li>
+        <li className='p__opensans'><a href='#about'>About</a></li>
+        <li className='p__opensans'><a href='#menu'>Menu</a></li>
+        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        <li className='p__opensans'><a href='#contact'>Contact</a></li>
+    </ul>
+  )
+}
